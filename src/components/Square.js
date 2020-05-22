8475ae2b304cabc8a80e1563ea031b9b43dccf6d
Sarahmarie D Hollenstein <sholle7@gmail.com>
2020-05-22 03:25:40 -0500

    :tada: First commit

---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+/** @format */
+
+import React from 'react';
+
+export default function Square(props) {
+	return (
+		<button className="square" onClick={props.onClick}>
+			{props.value}
+		</button>
+	);
+}
